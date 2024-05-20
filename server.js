b45a34c1e924db7aae809be70c9183c2cbf825c9
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-20 10:47:01 +0200

    import dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import expressListEndpoints from "express-list-endpoints";
 import booksData from "./data/books.json";
 
@@ -8,6 +9,8 @@ const mongoUrl =
   process.env.MONGO_URL || "mongodb://localhost/project-mongo-books";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+dotenv.config()
 
 const { Schema } = mongoose;
 
